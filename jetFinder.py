ce56f79ecbfdccf74c489ea665f02f566171e120
matias <mantinanmatias@gmail.com>
2024-04-11 13:31:22 -0500

    jetfinder add variables

--- jetFinder.py
+++ jetFinder.py
@@ -60,14 +60,19 @@ if __name__ == "__main__":
         jet_phi = ctypes.c_double()
         jet_mass = ctypes.c_double()
         jet_r_inv = ctypes.c_double()
+        #jet_displacement = ctypes.c_double()
         jet_nparticles = ctypes.c_double()
+        jet_isdark = ctypes.c_bool()
         event_index = ctypes.c_int()
+        
         jet_tree.Branch("pt", jet_pt, "pt/D")
         jet_tree.Branch("eta", jet_eta, "eta/D")
         jet_tree.Branch("phi", jet_phi, "phi/D")
         jet_tree.Branch("mass", jet_mass, "mass/D")
         jet_tree.Branch("r_inv",jet_r_inv,"r_inv/D")
+        #jet_tree.Branch("displacement",jet_displacement,"displacement/D")
         jet_tree.Branch("nparticles",jet_nparticles,"nparticles/I")
+        jet_tree.Branch("isdark",jet_isdark,"isdark\B")
         jet_tree.Branch("event_index", event_index, "event_index/I")
 
         # Create a TTree to store constituent particles
@@ -76,12 +81,32 @@ if __name__ == "__main__":
         particle_eta = ctypes.c_double()
         particle_phi = ctypes.c_double()
         particle_mass = ctypes.c_double()
+        particle_prod_x = ctypes.c_double()
+        particle_prod_y = ctypes.c_double()
+        particle_prod_z = ctypes.c_double()
+        particle_prod_t = ctypes.c_double()
+        particle_end_x = ctypes.c_double()
+        particle_end_y = ctypes.c_double()
+        particle_end_z = ctypes.c_double()
+        particle_end_t = ctypes.c_double()
+        
+        particle_isdark = ctypes.c_bool()
         jet_index = ctypes.c_int()
         particle_id = ctypes.c_int()
+
         particle_tree.Branch("pt", particle_pt, "pt/D")
         particle_tree.Branch("eta", particle_eta, "eta/D")
         particle_tree.Branch("phi", particle_phi, "phi/D")
         particle_tree.Branch("mass", particle_mass, "mass/D")
+        particle_tree.Branch("prod_x",particle_prod_x,"prod_x/D")
+        particle_tree.Branch("prod_y",particle_prod_y,"prod_y/D")
+        particle_tree.Branch("prod_z",particle_prod_z,"prod_z/D")
+        particle_tree.Branch("prod_t",particle_prod_t,"prod_t/D")
+        particle_tree.Branch("end_x",particle_end_x,"end_x/D")
+        particle_tree.Branch("end_y",particle_end_y,"end_y/D")
+        particle_tree.Branch("end_z",particle_end_z,"end_z/D")
+        particle_tree.Branch("end_t",particle_end_t,"end_t/D")
+        particle_tree.Branch("isdark",particle_isdark,"isdark\B")
         particle_tree.Branch("pid", particle_id, "pid/I")
         particle_tree.Branch("jet_index", jet_index, "jet_index/I")
         particle_tree.Branch("event_index", event_index, "event_index/I")
@@ -148,7 +173,6 @@ if __name__ == "__main__":
                 njets += 1
 
 
-
             
 
             ## Check what dark particles are in the jets
@@ -164,6 +188,8 @@ if __name__ == "__main__":
                     ndark = 0
                     ndark_stable = 0
                     nparticles = 0
+                    jet_isdark = False
+
                     for particle in event.particles:
                         # decide which particles to keep
                         #accept = (particle.status == 1) # final state particle
@@ -175,9 +201,21 @@ if __name__ == "__main__":
                         if not accept:
                             continue
     
-                        
                         if inJet(particle,jet,0.4):
             
+                            particle_isdark = False
+                            nparticles += 1
+                            if abs(particle.pid) in [4900113,4900111,4900101,4900021]:
+                                jet_isdark = True
+                                particle_isdark = True
+                                ndark +=1
+                                print(f"{particle.pid} is in jet")
+                                if particle.status ==1:
+                                    ndark_stable +=1
+
+                            
+                            
+                            
                             particle_pt = particle.momentum.pt()
                             particle_eta = particle.momentum.eta()
                             particle_phi = particle.momentum.phi()
@@ -186,14 +224,37 @@ if __name__ == "__main__":
                             event_index = ievt
                             jet_index = jet_index
 
+
+                            prod_vertex = particle.production_vertex
+                            end_vertex = particle.end_vertex
+
+
+
+                            if not prod_vertex is None:
+                                particle_prod_x = prod_vertex.position.x
+                                particle_prod_y = prod_vertex.position.y
+                                particle_prod_z = prod_vertex.position.z
+                                particle_prod_t = prod_vertex.position.y
+                            else:
+                                particle_prod_x = 0
+                                particle_prod_y = 0
+                                particle_prod_z = 0
+                                particle_prod_t = 0
+                                
+
+                            if not end_vertex is None:
+                                particle_end_x = end_vertex.position.x
+                                particle_end_y = end_vertex.position.y
+                                particle_end_z = end_vertex.position.z
+                                particle_end_t = end_vertex.position.y
+                            else:
+                                particle_end_x = 0
+                                particle_end_y = 0
+                                particle_end_z = 0
+                                particle_end_t = 0
+
+
                             particle_tree.Fill()
-            
-                            nparticles += 1
-                            if abs(particle.pid) in [4900113,4900111,4900101,4900021]:
-                                ndark +=1
-                                print(f"{particle.pid} is in jet")
-                                if particle.status ==1:
-                                    ndark_stable +=1
 
                     #print(f"{nparticles} particles in jets")
 
@@ -212,6 +273,8 @@ if __name__ == "__main__":
                     jet_mass = jet.m()
                     jet_r_inv = r_inv
                     jet_nparticles = nparticles
+                    
+                    
                                         
 
                     jet_tree.Fill()
